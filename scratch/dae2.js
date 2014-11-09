214a90b9f17ae0b17b761c3ada299fd106ea87da
Derek <sleepytonic@gmail.com>
2014-11-09 13:13:38 -0500

    remove sandbox code

--- scratch/dae2.js
+++
@@ -1,7 +0,0 @@
-function init(e){
-	console.log('Initial Load event', this, arguments);
-
-   // if ( window.svgDocument == null )
-        window.svgDocument = evt.target.ownerDocument;
-        document.aa= 'a';
-}
